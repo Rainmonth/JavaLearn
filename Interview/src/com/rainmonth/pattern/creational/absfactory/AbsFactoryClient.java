96a62685ebf3c65ce95177fec62681f8f9c164d1
张豪成 <zhanghaocheng@hhdd.com>
2021-07-12 18:13:43 +0800

    完善抽象工厂模式

--- Interview/src/com/rainmonth/pattern/creational/absfactory/AbsFactoryClient.java
+++ Interview/src/com/rainmonth/pattern/creational/absfactory/AbsFactoryClient.java
@@ -1,11 +1,22 @@
 package com.rainmonth.pattern.creational.absfactory;
 
+import com.rainmonth.pattern.creational.absfactory.factory.AppleComputerFactory;
+import com.rainmonth.pattern.creational.absfactory.factory.ComputerFactory;
+import com.rainmonth.pattern.creational.absfactory.factory.HuaweiComputerFactory;
+
 /**
  * @author randy
  * @date 2021/7/12 5:18 下午
  */
 public class AbsFactoryClient {
     public static void main(String[] args) {
+        ComputerFactory computerFactory = new AppleComputerFactory();
+
+        computerFactory.computerInfo();
+
+        ComputerFactory computerFactory1 = new HuaweiComputerFactory();
+
+        computerFactory1.computerInfo();
 
     }
 }
